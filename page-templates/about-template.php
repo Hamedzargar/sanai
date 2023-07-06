e70e8ea82aa862c726d778360e4bac6e81aae1da
alirezajahan13 <107421412+alirezajahan13@users.noreply.github.com>
2023-07-06 12:32:38 +0330

    edit #2 About & Contact (#4)
    * create About & Contact Page
    
    * edit #2 About & Contact

--- page-templates/about-template.php
+++ page-templates/about-template.php
@@ -12,13 +12,13 @@ get_header();
 
 <div class="aboutUsPageBack extraHighMargined">
     <div class="aboutUsPage mainView">
-        <div class="generalHeading bigHeading highMargined centeredText">
+        <div class="generalHeading highMargined centeredText">
             <h1>درباره ما</h1>
             <span>کلینیک سنایی</span>
         </div>
         <div class="aboutUsFirstSection">
             <div class="aboutUsFirstRight">
-                <img src="<?php bloginfo('template_directory') ?>/img/11.jpg" alt="">
+                <img class="highRadius" src="<?php bloginfo('template_directory') ?>/img/11.jpg" alt="">
             </div>
             <div class="aboutUsFirstLeft">
                 <div class="aboutUsFirstLeftHead generalHeading highMargined">
@@ -34,40 +34,28 @@ get_header();
                 </a>
             </div>
         </div>
+        <div class="gradient1 highRadius mediumPadding highMargined">
+            <p class="noMargin justified">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+        </div>
         <div class="homePageServiceBoxParent highMargined">
             <div class="homePageServiceBox mainView">
-                <div class="serviceBoxParent box1">
+                <div class="serviceBoxParent highRadius box1">
                     <img src="http://localhost/sanai/wp-content/themes/sanai/img/bone-.png" alt="">
-                    <h3>درمان درد شانه</h3>
-                    <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
-                    <a href="#" class="btnHasntBg generalBtn">
-                        <span>دریافت نوبت</span>
-                        <span>
-                            <svg height="20" width="20" fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330 330" xml:space="preserve"><path d="M15 180h263.787l-49.394 49.394c-5.858 5.857-5.858 15.355 0 21.213C232.322 253.535 236.161 255 240 255s7.678-1.465 10.606-4.394l75-75c5.858-5.857 5.858-15.355 0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213 0-5.858 5.857-5.858 15.355 0 21.213L278.787 150H15c-8.284 0-15 6.716-15 15s6.716 15 15 15z"></path></svg>
-                        </span>
-                    </a>
+                    <h3 class="mediumWeight">درمان درد شانه</h3>
+                    <p class="smallerText mainParagraph">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
+                    <a class="generalButton outlineButton noBorderButton mediumMargined" href="#">دریافت نوبت</a>
                 </div>
-                <div class="serviceBoxParent box2">
+                <div class="serviceBoxParent highRadius box2">
                     <img src="http://localhost/sanai/wp-content/themes/sanai/img/bone-.png" alt="">
-                    <h3>درمان درد شانه</h3>
-                    <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
-                    <a href="#" class="btnHasntBg generalBtn">
-                        <span>دریافت نوبت</span>
-                        <span>
-                            <svg height="20" width="20" fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330 330" xml:space="preserve"><path d="M15 180h263.787l-49.394 49.394c-5.858 5.857-5.858 15.355 0 21.213C232.322 253.535 236.161 255 240 255s7.678-1.465 10.606-4.394l75-75c5.858-5.857 5.858-15.355 0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213 0-5.858 5.857-5.858 15.355 0 21.213L278.787 150H15c-8.284 0-15 6.716-15 15s6.716 15 15 15z"></path></svg>
-                        </span>
-                    </a>
+                    <h3 class="mediumWeight">درمان درد شانه</h3>
+                    <p class="smallerText mainParagraph">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
+                    <a class="generalButton outlineButton noBorderButton mediumMargined" href="#">دریافت نوبت</a>
                 </div>
-                <div class="serviceBoxParent box3">
+                <div class="serviceBoxParent highRadius box3">
                     <img src="http://localhost/sanai/wp-content/themes/sanai/img/bone-d.png" alt="">
-                    <h3>درمان درد شانه</h3>
-                    <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
-                    <a href="#" class="btnHasntBg generalBtn">
-                        <span>دریافت نوبت</span>
-                        <span>
-                            <svg height="20" width="20" fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330 330" xml:space="preserve"><path d="M15 180h263.787l-49.394 49.394c-5.858 5.857-5.858 15.355 0 21.213C232.322 253.535 236.161 255 240 255s7.678-1.465 10.606-4.394l75-75c5.858-5.857 5.858-15.355 0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213 0-5.858 5.857-5.858 15.355 0 21.213L278.787 150H15c-8.284 0-15 6.716-15 15s6.716 15 15 15z"></path></svg>
-                        </span>
-                    </a>
+                    <h3 class="mediumWeight">درمان درد شانه</h3>
+                    <p class="smallerText mainParagraph">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
+                    <a class="generalButton outlineButton noBorderButton mediumMargined" href="#">دریافت نوبت</a>
                 </div>
             </div>
         </div>
